cc5bee113ef74d5928964d420145f906df402f51
amitnihalani <ami.nihalani@gmail.com>
2015-02-21 15:50:05 -0500

    Union Operator
    Code for Union.

--- src/edu/buffalo/cse562/operators/OperatorTest.java
+++ src/edu/buffalo/cse562/operators/OperatorTest.java
@@ -2,6 +2,8 @@ package edu.buffalo.cse562.operators;
 
 import java.io.File;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
 
 import edu.buffalo.cse562.utility.Utility;
 import net.sf.jsqlparser.expression.Expression;
@@ -9,11 +11,44 @@ import net.sf.jsqlparser.statement.select.SelectExpressionItem;
 
 public class OperatorTest {
 
-	public static void execute(File file, String tableName, Expression condition,ArrayList<SelectExpressionItem> list){
+	public static void executeSelect(File file, String tableName, Expression condition,ArrayList<SelectExpressionItem> list){
 		Operator oper = new ReadOperator(file, tableName);
 		oper= new SelectionOperator(oper, Utility.tables.get(tableName), condition);
 		oper = new ProjectOperator(oper, list, tableName);
 		dump(oper);
+	}
+	
+	public static void executeUnion(ArrayList<ArrayList<Object>> selectStatementsParameters){
+		ArrayList<Operator> oper = new ArrayList<Operator>();
+		
+		for(ArrayList<Object> statementParameters : selectStatementsParameters){
+			Operator o = new ReadOperator(new File((String)statementParameters.get(0)), (String)statementParameters.get(1));
+			if((Expression)statementParameters.get(2) != null){
+				o = new SelectionOperator(o, Utility.tables.get((String)statementParameters.get(1)), (Expression)statementParameters.get(2));
+			}
+			
+			o = new ProjectOperator(o, (ArrayList<SelectExpressionItem>)statementParameters.get(3), (String)statementParameters.get(1));
+		    oper.add(o);
+		}
+		dumpUnion(oper);
+	}
+	
+	public static void dumpUnion(ArrayList<Operator> oper){
+		HashSet<String> unionTuples = new HashSet<String>();
+		for(Operator op : oper){
+			Object[] row = op.readOneTuple();
+			while(row != null){
+				String tuple = "";
+				for(Object col: row){
+					tuple += (col.toString()+" | ");
+				}
+				if(!unionTuples.contains(tuple)){
+					unionTuples.add(tuple);
+					System.out.println(tuple);
+				}
+				row = op.readOneTuple();
+			}
+		}
 	}
 	
 	public static void dump(Operator input){
--- src/edu/buffalo/cse562/parsers/SelectParser.java
+++ src/edu/buffalo/cse562/parsers/SelectParser.java
@@ -2,6 +2,7 @@ package edu.buffalo.cse562.parsers;
 
 import java.io.File;
 import java.util.ArrayList;
+import java.util.List;
 
 import net.sf.jsqlparser.expression.Expression;
 import net.sf.jsqlparser.statement.Statement;
@@ -9,6 +10,7 @@ import net.sf.jsqlparser.statement.select.PlainSelect;
 import net.sf.jsqlparser.statement.select.Select;
 import net.sf.jsqlparser.statement.select.SelectBody;
 import net.sf.jsqlparser.statement.select.SelectExpressionItem;
+import net.sf.jsqlparser.statement.select.Union;
 import edu.buffalo.cse562.operators.OperatorTest;
 import edu.buffalo.cse562.utility.Utility;
 
@@ -23,14 +25,50 @@ public class SelectParser {
 		SelectBody body = ((Select) statement).getSelectBody();
 		
 		if(body instanceof PlainSelect){
-			String from = ((PlainSelect) body).getFromItem().toString();
-			Expression condition= ((PlainSelect) body).getWhere();
+		/*	String from = returnFromItem(body);
+			Expression condition= returnConditionItem(body);
 			ArrayList<SelectExpressionItem> list = (ArrayList<SelectExpressionItem>)
 					((PlainSelect) body).getSelectItems();
 			String dataFileName = from.toLowerCase() + ".dat";
-			dataFileName = Utility.dataDir.toString() + File.separator + dataFileName;
+			dataFileName = Utility.dataDir.toString() + File.separator + dataFileName; */
+			ArrayList<Object> parameters = getParameters(body);
 			
-			OperatorTest.execute(new File(dataFileName), from, condition,list);
+			//OperatorTest.executeSelect(new File(dataFileName), from, condition,list);
+			OperatorTest.executeSelect(new File((String)parameters.get(0)), 
+					(String)parameters.get(1), 
+					(Expression)parameters.get(2),
+					(ArrayList<SelectExpressionItem>)parameters.get(3));
+		}
+		else if(body instanceof Union){
+			ArrayList<PlainSelect> plainSelects = new ArrayList(((Union) body).getPlainSelects());
+			ArrayList<ArrayList<Object>> selectStatementsParameters = new ArrayList<ArrayList<Object>>(); 
+			
+			for(PlainSelect p : plainSelects){
+				selectStatementsParameters.add(getParameters(p));
+			}
+			OperatorTest.executeUnion(selectStatementsParameters);
 		}
 	}
+	
+	public static String returnFromItem(SelectBody body){
+		return ((PlainSelect) body).getFromItem().toString();
+	}
+	
+	public static Expression returnConditionItem(SelectBody body){
+		return ((PlainSelect) body).getWhere();
+	}
+	
+	
+	
+	public static ArrayList<Object> getParameters(SelectBody body){
+		//list of parameters in the sequence - From Item, Condition Item,  Select Items, DataFileName
+		ArrayList<Object> parameters = new ArrayList<Object>();
+		parameters.add(Utility.dataDir.toString() + File.separator + ((PlainSelect) body).getFromItem().toString() + ".dat");
+		parameters.add(((PlainSelect) body).getFromItem().toString());
+		parameters.add(((PlainSelect) body).getWhere());
+		parameters.add((ArrayList<SelectExpressionItem>)((PlainSelect) body).getSelectItems());		
+		return parameters;
+	}
+	
+	
 }
